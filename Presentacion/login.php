7645e6df674645f573b46e2bc2c4992c6ac604d5
David <dm.ambu@gmail.com>
2021-05-18 21:55:17 -0400

    login

--- Presentacion/login.php
+++ Presentacion/login.php
@@ -1,16 +1,32 @@
-<?php
-    //Require a atribos de conexión.
-    require_once "../persistencia/parametrosBD.php"; //Para que nos de los attributos de conexión.
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="main.css">
 
-    //Try catch apra control de excepciones
-    try{
-        //Objeto de conexion, que vien de la clase PDO, que es un objeto BASE de php, que nos permite conectarno a la bbdd
-        //mysql:host=$parametroHost;
-        $conn = new PDO("mysql:host=$host;dbname=$nombreBaseDatos", $usuario,$password);
-        echo "Por fin estoy coenctado a mi base de datos";
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+    <title>Login</title>
+</head>
+<body class="login">
+<div class="container">
+    <div class="container">
+        <h3>Iniciar sesión</h3>
+        <form action="">
+            <input class="input" type="text" name="" id="" placeholder="Usuario">
+            <input class="input" type="password" name="" id="" placeholder="Contraseña">
+        </form>
+    </div>
+</div>
+    
+</body>
 
-    }catch(PDOException $pe){ 
-        echo "No se conectó por la ....!!!". $pe->getMessage();
-    }
-
-?>
+<footer>
+    <div class="container">
+    <p>©2021 Repi Elramo. Desarrollado por The Vapers. v.0.0</p>
+    </div>
+</footer>
+</html>
