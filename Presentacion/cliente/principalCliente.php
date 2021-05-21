f3d2f412c8a378c1de883e81cb56e28403e7064a
David <dm.ambu@gmail.com>
2021-05-20 23:39:04 -0400

    Mantenimiento Clientes (CRU)

--- Presentacion/cliente/principalCliente.php
+++ Presentacion/cliente/principalCliente.php
@@ -36,8 +36,8 @@
             .contenedorTabla{
                 padding-top: 15px;
                 width: 100%;
-                padding-left: 4rem;
-                padding-right: 4rem;
+                padding-left: 2.5rem;
+                padding-right: 2.5rem;
             }
             
         </style>
@@ -90,8 +90,18 @@
                     <td><?php echo $cliente->getTelefono();?></td>
                     <td><?php echo $cliente->getEmail();?></td>
                     <td>
-                        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#ventanaModal"><i class="bi bi-pencil-fill"></i></button>
-                        <span class="btn btn-danger"><i class="bi bi-trash2-fill"></i></span>
+                        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#modalEditar" 
+                                data-rut='<?php echo $cliente->getRutCliente()?>'
+                                data-dv='<?php echo $cliente->getDvCliente()?>'
+                                data-razon='<?php echo $cliente->getNombRazonSocial()?>'
+                                data-giro='<?php echo $cliente->getGiroCliente()?>'
+                                data-direccion='<?php echo $cliente->getDireccion()?>'
+                                data-comuna='<?php echo $cliente->getComuna()?>'
+                                data-ciudad='<?php echo $cliente->getCiudad()?>'
+                                data-telefono='<?php echo $cliente->getTelefono()?>'
+                                data-email='<?php echo $cliente->getEmail()?>'><i class="bi bi-pencil-fill"></i></button>
+                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalEliminar" 
+                                data-rut='<?php echo $cliente->getRutCliente()?>'><i class="bi bi-trash2-fill"></i></button>
                     </td>
                 </tr>
                 <?php
@@ -106,12 +116,12 @@
           
     </table>
 
-    <!-- ventana modal -->
+    <!-- ventana modal registrar-->
     <div class="modal fade" id="ventanaModal" tabindex="-1" role="dialog" aria-labelledby="tituloVentana" aria-hidden="true">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5><i class="bi bi-bookmark-plus-fill"></i> Nuevo Cliente</h5>
+                    <h5><i class="bi bi-bookmark-plus-fill"></i> Registrar Nuevo Cliente</h5>
                     <button class="close" data-dismiss="modal" aria-label="cerrar">
                         <span aria-hidden="true">&times;</span>
                     </button>
@@ -177,6 +187,115 @@
     </div>
 
     </div>
+
+    <!-- ventana modal editar-->
+    <div class="modal fade" id="modalEditar" tabindex="-1" role="dialog" aria-labelledby="tituloVentana" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5><i class="bi bi-pencil-square"></i> Modificar Cliente</h5>
+                    <button class="close" data-dismiss="modal" aria-label="cerrar">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="../../Controlador/controladorCliente.php" method="POST">
+                        <div class="container-fluid">
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col col-lg-9">
+                                        <input class="form-control" type="text" readonly="true" name="Rut" id="txtRut" placeholder="Ingrese su Rut">                        
+                                    </div>
+                                    <div class="col col-xs-3">                        
+                                        <input class="form-control l" type="text" readonly="true" name="Dv" id="txtDv" placeholder="Dv">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="container-fluid">
+                            <div class="form-group">
+                                <input class="form-control" type="text" name="Razon" id="txtRazon" placeholder="Razón Social">
+                            </div>
+                            <div class="form-group">
+                                <input class="form-control" type="text" name="Giro" id="txtGiro" placeholder="Giro">
+                            </div>
+                            <div class="form-group">
+                                <input class="form-control" type="text" name="Direccion" id="txtDireccion" placeholder="Direccion">
+                            </div>               
+                        </div>
+                        <div class="container-fluid">
+                            <div class="form-group">
+                                    <div class="row">
+                                        <div class="col col-lg-6">
+                                            <input class="form-control" type="text" name="Comuna" id="txtComuna" placeholder="Comuna">                        
+                                        </div>
+                                        <div class="col col-lg-6">                        
+                                            <input class="form-control l" type="text" name="Ciudad" id="txtCiudad" placeholder="Ciudad">
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                        <div class="container-fluid">
+                            <div class="form-group">
+                                    <div class="row">
+                                        <div class="col col-lg-6">
+                                            <input class="form-control" type="tel" name="Telefono" id="txtTelefono" placeholder="Telefono">                        
+                                        </div>
+                                        <div class="col col-lg-6">                        
+                                            <input class="form-control l" type="email" name="Email" id="txtEmail" placeholder="E-mail">
+                                        </div>
+                                    </div>
+                            </div>
+                        </div>
+                        <div class="container-fluid">
+                        <div class="form-group">
+                            <button class="btn btn-dark col-lg-12" name="modificar" type="submit">Modificar</button>
+                        </div>
+                    </div>                        
+                    </form>
+                </div>                
+            </div>
+        </div>
+    </div>
+
+    <!-- ventana modal Eliminar-->
+    <div class="modal fade" id="modalEliminar" tabindex="-1" role="dialog" aria-labelledby="tituloVentana" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5><i class="bi bi-x-octagon-fill"></i> Eliminar Cliente</h5>
+                    <button class="close" data-dismiss="modal" aria-label="cerrar">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="../../Controlador/controladorCliente.php" method="POST">
+                        <div class="container-fluid">
+                            <div class="form-group">
+                                <center>
+                                <label for="">¿Seguro que desea Eliminar los registros?</label>
+                                </center>
+                            </div>
+                        </div>  
+                        <div class="container-fluid">
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col col-lg-6">
+                                        <button class="btn btn-success col-lg-12" name="eliminar" type="submit">Si</button>                        
+                                    </div>
+                                    <div class="col col-xs-6">                        
+                                    <button class="btn btn-danger col-lg-12" type="submit">no</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>                        
+                    </form>
+                </div>                
+            </div>
+        </div>
+    </div>
+
+    </div>
     <!-- script de jquery -->
     <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
 
@@ -190,3 +309,7 @@
     <script type="text/javascript" src="../main.js"></script>
 </body>
 </html>
+
+
+                            
+                                
